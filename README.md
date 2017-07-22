b3518192f558f45a2296fed8ffefdfc27e0e348a
Govind.Kalyanaraman <govind.kalyana@gmail.com>
2017-07-22 17:12:56 +0100

    Truffle artefacts and Smart Contract

--- README.md
+++ README.md
@@ -4,11 +4,4 @@
 As an insurance customer there is no easy way to demonstrate your personal claim history.
 RESULT: There is no way to prove you are a good quality low risk customer to an insurance provider.
 
-By transfering No Claim Bonus (NCB) tokens into a policy holders account when a policy expires we will intrinsically store a value representing a customers risk profile. 
-
-Insurance provides have the option to offer a discounted premium in exchange for NCB tokens.  
-The value of an NCB token is determined by the insurance provider and can be used to attract customers to spesific lines of business.
-
-### NCB TOKENS ARE NON-TRADABLE
-Tokens are created when a policy expires and destroyed when redeemed.
-
+By transfering No Claim Bouns (NCB) tokens into a policy holders account when a policy expires.  
---
+++ contracts/TokenManager.sol
@@ -0,0 +1,50 @@
+pragma solidity ^0.4.8;
+
+contract TokenManager {
+
+	struct Policy {
+		address consumer;
+		uint256 policyNo;
+		uint256 duration;
+		uint256 totalPremium;
+		bool claimMade;
+	}
+
+	mapping(address => uint256) consumerTokensMap;
+	mapping(uint256 => Policy) policyNumberPolicyMap;
+
+	function policyPurchased(uint256 _policyNo, address _consumer, uint256 _duration, uint256 _totalPremium){
+
+		policyNumberPolicyMap[_policyNo] =  Policy({
+				consumer : _consumer,
+				policyNo : _policyNo,
+				duration : _duration,
+				totalPremium : _totalPremium,
+				claimMade : false		
+			});
+
+	}
+
+	function policyClaimMade(uint256 _policyNo){
+		Policy policy = policyNumberPolicyMap[_policyNo];
+		policy.claimMade  = true;
+	}
+
+	function policyCancelled(uint256 _policyNo, uint256 _totalPremiumPaid){
+		Policy policy = policyNumberPolicyMap[_policyNo];
+	
+		if(!policy.claimMade){
+			address consumer = policy.consumer;
+			consumerTokensMap[consumer] += _totalPremiumPaid/100;
+		}
+	}
+
+	function policyExpired(uint256 _policyNo){
+		Policy policy = policyNumberPolicyMap[_policyNo];
+	
+		if(!policy.claimMade){
+			address consumer = policy.consumer;
+			consumerTokensMap[consumer] += policy.totalPremium/100;
+		}
+	}
+}
